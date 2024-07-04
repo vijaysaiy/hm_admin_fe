17903b4646d0fd3ebaeff4180ce5e83f16f4e8e8
Vijaysai Yekbote <vijaysaiyekbote@gmail.com>
2024-07-05 01:11:01 +0530

    app setup and layout setup

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,153 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Bell,
+  CalendarPlus2,
+  CircleUser,
+  LayoutDashboard,
+  Menu,
+  Package2,
+  Stethoscope,
+  Users,
+} from "lucide-react";
+import { Link, Outlet, useLocation } from "react-router-dom";
+
+const navItemIconClass = "h-4 w-4";
+
+const linkToTitle = {
+  "/dashboard": "Dashboard",
+  "/patients": "Patients",
+  "/doctors": "Doctors",
+  "/appointments": "Appointments",
+};
+
+const navItems = [
+  {
+    label: "Dashboard",
+    icon: <LayoutDashboard className={navItemIconClass} />,
+    link: "/dashboard",
+  },
+  {
+    label: "Patients",
+    icon: <Users className={navItemIconClass} />,
+    link: "/patients",
+  },
+  {
+    label: "Doctors",
+    icon: <Stethoscope className={navItemIconClass} />,
+    link: "/doctors",
+  },
+  {
+    label: "Appointments",
+    icon: <CalendarPlus2 className={navItemIconClass} />,
+    link: "/appointments",
+  },
+];
+
+const DashboardLayout = () => {
+  const location = useLocation();
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link to="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">HMS Admin</span>
+            </Link>
+            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+              <Bell className="h-4 w-4" />
+              <span className="sr-only">Toggle notifications</span>
+            </Button>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              {navItems.map((item) => (
+                <Link
+                  to={item.link}
+                  className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary  ${
+                    item.link === location.pathname
+                      ? "bg-gray-200"
+                      : "text-muted-foreground"
+                  }`}
+                >
+                  {item.icon}
+                  {item.label}
+                </Link>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                {navItems.map((item) => (
+                  <Link
+                    to={item.link}
+                    className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary "
+                  >
+                    {item.icon}
+                    {item.label}
+                  </Link>
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
+          <div className="w-full flex-1">
+            <h1 className="text-lg font-semibold md:text-2xl">
+              {linkToTitle[location.pathname as keyof typeof linkToTitle]}
+            </h1>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <div
+            className="flex flex-1 items-center justify-center rounded-md border shadow-sd"
+            x-chunk="dashboard-02-chunk-1"
+          >
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
