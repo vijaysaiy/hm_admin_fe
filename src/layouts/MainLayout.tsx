363718c239f88b62994b5cefc56e97a56fa4a8c1
Vijaysai Yekbote <vijaysaiyekbote@gmail.com>
2024-07-05 01:51:37 +0530

    Reduced side bar width

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -55,10 +55,10 @@ const navItems = [
 const DashboardLayout = () => {
   const location = useLocation();
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[240px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-4">
             <Link to="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
               <span className="">HMS Admin</span>
